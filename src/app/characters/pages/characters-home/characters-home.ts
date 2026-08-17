d889306c55c720fcc1526a7a9044066b3bc4bae3
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-08-17 14:45:27 -0400

    Llenada vista CharactersHome

--- src/app/characters/pages/characters-home/characters-home.ts
+++ src/app/characters/pages/characters-home/characters-home.ts
@@ -1,9 +1,14 @@
+import { TitleCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-characters-home',
-  imports: [],
+  imports: [RouterLink, TitleCasePipe],
   templateUrl: './characters-home.html',
   styleUrl: './characters-home.css',
 })
-export class CharactersHome {}
+export class CharactersHome {
+
+  houses = ['gryffindor', 'slytherin', 'hufflepuff', 'ravenclaw'];
+}
