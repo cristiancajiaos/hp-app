997c795392f8ca633c8d18e8d108737ff223026f
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-08-18 15:36:15 -0400

    Implementado caché en la vista MainSpells

--- src/app/spells/services/spell-service.ts
+++ src/app/spells/services/spell-service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Service } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable, of, tap } from 'rxjs';
 import { Spell } from '../interfaces/spell';
 import { environment } from '../../../environments/environment.development';
 
@@ -9,7 +9,14 @@ export class SpellService {
 
   private http = inject(HttpClient);
 
-  getSpells(): Observable<Spell[]> {
-    return this.http.get<Spell[]>(`/spells`);
+  spellsCache = new Map<string, Spell[]>();
+
+  getSpells(): Observable<Spell[] | undefined> {
+    if (this.spellsCache.has('spells')) {
+      return of(this.spellsCache.get('spells'));
+    }
+    return this.http.get<Spell[]>(`/spells`).pipe(
+      tap(spells => this.spellsCache.set('spells', spells))
+    )
   }
 }
